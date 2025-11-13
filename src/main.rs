cc6f40f6bdf70cd87e5b99034dc843c73f44eb51
Remi Van Keisbelck <remi@rvkb.com>
2025-11-13 17:18:27 +0100

    rendering

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,13 @@ mod build_status;
 mod circle_ci;
 mod config;
 mod utils;
+mod rendering;
 
 use std;
 
 use crate::{
-    build_status::{BuildStatus, TimeInfo},
-    config::{env_replacer, load_config},
+    build_status::{BuildStatus},
+    config::{BuildConfig, env_replacer, load_config}, rendering::render_rows,
 };
 
 static CONFIG_FILE: &str = ".bwatch.json";
@@ -24,43 +25,19 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .into_iter()
         .map(async |x| x.fetch().await.map(|r| (r, x)));
     let joined = futures::future::join_all(futures).await;
+
+    let mut rows: Vec<(&BuildConfig, &BuildStatus)> = Vec::new();    
     for r in joined.iter() {
         match r {
-            Ok((
-                BuildStatus {
-                    status,
-                    time_info,
-                    url,
-                },
-                build_config,
-            )) => {
-                let title = build_config.get_title();
-                let elapsed: String = match time_info {
-                    Some(TimeInfo {
-                        completed_at,
-                        duration_secs,
-                    }) => {
-                        let parsed_date = chrono::DateTime::parse_from_rfc3339(completed_at)
-                            .map(|parsed_date| parsed_date.format("%Y-%m-%d %H:%M:%S").to_string())
-                            .ok()
-                            .unwrap_or(completed_at.to_string());
-                        let secs: u64 = u64::try_from(*duration_secs).unwrap();
-                        let d = std::time::Duration::from_secs(secs);
-                        let pretty = pretty_duration::pretty_duration(&d, None);
-                        format!("📅 {} | 🕑 {}", parsed_date, pretty)
-                    }
-                    None => String::from("📅 N/A | 🕑 N/A"),
-                };
-                let status = match status {
-                    build_status::Status::Green => '✅',
-                    build_status::Status::Red => '❌',
-                };
-                println!("{status} {title} | {url} | {elapsed}");
+            Ok((status, config)) => {
+                rows.push((config, status));
             }
             Err(e) => {
                 println!("Error: {}", e);
             }
         }
     }
+    render_rows(rows);
+
     Ok(())
 }
---
+++ src/rendering.rs
@@ -0,0 +1,75 @@
+use crate::{build_status::{BuildStatus, Status}, config::BuildConfig};
+
+pub struct RowData {
+    status: char,
+    title: String,
+    url: String,
+    completed_at: String,
+    duration: String
+}
+
+const STATUS_GREEN: char = '✅';
+const STATUS_RED: char = '❌';
+
+fn status_to_string(status: &Status) -> char {
+    match status {
+        Status::Green => STATUS_GREEN,
+        Status::Red => STATUS_RED,
+    }
+}
+
+fn pad_str(s: &str, i: usize) -> String {
+    format!("{:width$}", s, width=i)
+}
+
+pub fn render_rows(rows: Vec<(&BuildConfig, &BuildStatus)>) -> () {
+    let mut res: Vec<RowData> = Vec::new();
+    let mut max_title = 0;
+    let mut max_url = 0;
+    let mut max_completed_at = 0;
+    let mut max_duration = 0;
+    
+    for (config,status) in rows.into_iter() {
+        max_title = std::cmp::max(max_title, config.get_title().len());
+        max_url = std::cmp::max(max_url, status.url.len());
+
+        let (completed_at, duration) = status.time_info.as_ref()
+            .map(|time_info| {
+                let parsed_date = chrono::DateTime::parse_from_rfc3339(&time_info.completed_at)
+                    .map(|parsed_date| parsed_date.format("%Y-%m-%d %H:%M:%S").to_string())
+                    .ok()
+                    .unwrap_or(time_info.completed_at.to_string());
+                let secs: u64 = u64::try_from(time_info.duration_secs).unwrap();
+                let d = std::time::Duration::from_secs(secs);
+                let pretty = pretty_duration::pretty_duration(&d, None);
+                (parsed_date, pretty)
+            })
+            .unwrap_or((String::new(), String::new()));            
+
+        max_completed_at = std::cmp::max(max_completed_at, completed_at.len());
+        max_duration = std::cmp::max(max_duration, duration.len());
+
+        res.push(RowData {
+            status: status_to_string(&status.status),
+            title: config.get_title(),
+            url: status.url.to_string(),
+            completed_at,
+            duration,
+        });
+    }
+
+    res.sort_by(|a, b| a.title.cmp(&b.title));
+    for row in res.into_iter() {
+        let status = row.status;
+        let title = pad_str(&row.title, max_title);
+        let url = pad_str(&row.url, max_url);
+        let completed_at = pad_str(&row.completed_at, max_completed_at);
+        let duration = pad_str(&row.duration, max_duration);
+        let row = format!(            
+            "{status} {title} | {url} | {completed_at} | {duration}"
+        );
+        println!("{row}");
+    }
+
+    ()
+}
